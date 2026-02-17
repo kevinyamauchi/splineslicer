50cd7b0ff031ed7a5df7736aaa19137fc7b914c3
Kevin <kevin.yamauchi@gmail.com>
2026-02-17 13:11:38 +0100

    convert to npe2

--- src/splineslicer/_dock_widget.py
+++ src/splineslicer/_dock_widget.py
@@ -5,8 +5,10 @@ It implements the ``napari_experimental_provide_dock_widget`` hook specification
 see: https://napari.org/docs/dev/plugins/hook_specifications.html
 
 Replace code below according to your needs.
+
+Note: This file is no longer used with npe2. Widget contributions are now
+defined in the napari.yaml manifest file. This file is kept for reference only.
 """
-from napari_plugin_engine import napari_hook_implementation
 
 from .measure._qt_measure import QtMeasure
 from .skeleton._qt_skeletonize import QtSkeletonize
@@ -14,7 +16,6 @@ from .slice.slice import slice_image_from_file
 from .view.results_viewer import QtResultsViewer
 
 
-@napari_hook_implementation
 def napari_experimental_provide_dock_widget():
     # you can return either a single widget, or a sequence of widgets
     return [
