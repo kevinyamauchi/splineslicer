fixing rotation and channel loading in the boundary viewer

src/splineslicer/correction/_qt_correct.py
@@ -211,7 +211,7 @@ class QtBoundaryCorrection(QWidget):
         target_names_result_table = res[np.argsort(ind)]
         target_name = target_names_result_table[self.current_channel_index]
 
-        #target_name = self.stain_channeL_names[self.current_channel_index]
+        # target_name = self.stain_channeL_names[self.current_channel_index]
         slice_row = self.results_table.loc[
             (self.results_table["target"] == target_name) &
             (self.results_table["slice_index"] == slice_index)
@@ -414,8 +414,9 @@ class QtBoundaryReader(QWidget):
         )
 
     def _prepare_slices(self, image_path: str, results_table: pd.DataFrame):
-        hdf5_file = h5py.File(image_path)
-        stain_image = hdf5_file[list(hdf5_file.keys())[0]][1]
+        # hdf5_file = h5py.File(image_path)
+        with h5py.File(image_path) as hdf5_file:
+            stain_image = hdf5_file[list(hdf5_file.keys())[0]][:]
 
         if stain_image.ndim == 3:
             # make sure the image is 4D

src/splineslicer/rotate/_qt_rotator.py
@@ -9,9 +9,9 @@ from qtpy.QtCore import Qt
 from qtpy.QtWidgets import QComboBox, QVBoxLayout, QWidget, QPushButton
 from superqt.sliders import QLabeledSlider
 from skimage.transform import rotate
-import splineslicer
 from superqt.collapsible import QCollapsible
 from .rotations_utils import new_align_rotate
+from ..skeleton.binarize import binarize_image
 
 class QtUpdatedRotation(QWidget):
     def __init__(self, napari_viewer: napari.Viewer):
@@ -98,7 +98,7 @@ class QtUpdatedRotation(QWidget):
         im_seg = self._viewer.layers.selection.active.data
         binarized_im = np.zeros((im_seg.shape))
         for i, chan in enumerate(im_seg):
-            binarized_im[i,...] = splineslicer.skeleton.binarize.binarize_image(im = im_seg, channel=i, threshold=threshold)
+            binarized_im[i,...] = binarize_image(im = im_seg, channel=i, threshold=threshold)
         layer_type = 'image'
         metadata = {
             'name': 'binarized_image',

src/splineslicer/rotate/rotations_utils.py
@@ -1,11 +1,11 @@
 from typing import Any, Dict, List, Optional, TYPE_CHECKING
 from napari.layers import Image
 import numpy as np 
-import splineslicer
 import napari
 from scipy.signal import find_peaks
 from skimage.measure import label, regionprops
 import math 
+from ..measure.align_slices import rotate_stack
 
 
 def new_align_rotate(
@@ -34,10 +34,10 @@ def new_align_rotate(
         adapted_rotations = np.asarray(adapted_rotations)
 
     # rotate the segmentation
-    rotated_seg = splineslicer.measure.align_slices.rotate_stack(mask_layer.data[NT_segmentation_index,start_slice:end_slice,...], adapted_rotations)
+    rotated_seg = rotate_stack(mask_layer.data[NT_segmentation_index,start_slice:end_slice,...], adapted_rotations)
     rotated_stack_adapted = []
     for i, chan in enumerate(stain_layer.data[:,start_slice:end_slice,...]):
-        rotated_stack_adapted.append(splineslicer.measure.align_slices.rotate_stack(np.asarray(chan), adapted_rotations))
+        rotated_stack_adapted.append(rotate_stack(np.asarray(chan), adapted_rotations))
     
     # if no NT segmented or multiple segmented elements are found then the slice is not taken and axis 1 (nb of slices)
     # may become <100. needs to account for this 